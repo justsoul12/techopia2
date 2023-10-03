374f19cb64cd324a0e499031f27552630ee9f01f
alazaradane <alazargithub369@gmail.com>
2023-10-03 20:15:27 +0300

    Build NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react'
+import NavBar from './components/NavBar'
 
 
 function App() {
@@ -10,7 +11,9 @@ function App() {
         base: `'nav' 'main'`,
         lg: `'nav nav' 'aside main'` // 1024px
       }}>
-        <GridItem area='nav' bg={'coral'}>Nav</GridItem>
+        <GridItem area='nav'>
+          <NavBar />
+        </GridItem>
         <Show above='lg'>
           <GridItem area='aside' bg={'green'}>Aside</GridItem>
         </Show>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import { HStack, Image , Text} from "@chakra-ui/react"
+
+
+import logo from '../assets/Techopia.webp'
+
+
+const NavBar = () => {
+  return (
+    <HStack>
+        <Image src={logo} boxSize={'60px'}/>
+        <Text>NavBar</Text>
+    </HStack>
+  )
+}
+
+export default NavBar
