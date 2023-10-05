67bb583ec6aa5def2f9ae54ab8d1e7b1e0a7783f
alazaradane <alazargithub369@gmail.com>
2023-10-05 23:25:18 +0300

    Creating a genring data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,17 +10,17 @@ import GameCardSkeleton from './GameCardSkeleton';
 
 const GameGrid = () => {
 
-    const {games, error, isLoading} = useGames()
+    const {data, error, isLoading} = useGames()
     const skeletons = [1,2,3,4,5,6]
     
   return (
     <>
     {error && <Text>{error}</Text>}
-    <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing={10} padding={10}>
+    <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 3}} spacing={10} padding={10}>
         {isLoading && skeletons.map(Skeleton => <GameCardContainer>
           <GameCardSkeleton key={Skeleton}/>
         </GameCardContainer>)}
-        {games.map(game=> (<GameCardContainer>
+        {data.map(game=> (<GameCardContainer>
           <GameCard key={game.id} game={game}/>
         </GameCardContainer>))}
     </SimpleGrid>
